acaf98512bec1cc3d4a42ca06bbe0d7278283a06
Dhaval Chheda <dhaval@omneky.com>
2026-07-18 15:47:28 +0530

    changed healthcheck route message

--- routes/web.php
+++ routes/web.php
@@ -4,5 +4,5 @@ use Illuminate\Support\Facades\Route;
 
 # Healthcheck route for fleet hosting service
 Route::get('/', function () {
-    return "I am now on fleet"
+    return "I am now on fleet";
 });
