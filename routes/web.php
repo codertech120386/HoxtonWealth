3ccbf3b268940e198f358a8a4161804c32c045fa
Dhaval Chheda <dhaval@omneky.com>
2026-07-19 08:33:17 +0530

    added healthcheck route comment

--- routes/web.php
+++ routes/web.php
@@ -4,5 +4,5 @@ use Illuminate\Support\Facades\Route;
 
 # Healthcheck route for fleet hosting service
 Route::get('/', function () {
-    return "testing again after the fix as first refresh gave a 502";
+    return "testing to check 200's work immediately";
 });
