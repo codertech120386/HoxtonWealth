c34926ad36b91bcdea25c0fe55133e37bcf16008
Dhaval Chheda <dhaval@omneky.com>
2026-07-19 10:26:11 +0530

    added healthcheck route comment to check deploys endpoint

--- routes/web.php
+++ routes/web.php
@@ -4,5 +4,5 @@ use Illuminate\Support\Facades\Route;
 
 # Healthcheck route for fleet hosting service
 Route::get('/', function () {
-    return "testing to check 200's work immediately";
+    return "testing to deploys endpoint";
 });
